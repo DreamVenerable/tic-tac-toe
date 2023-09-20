3c85d184f63ec9cb855d3cc4851d98cf9076b3d8
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-21 00:00:26 +0200

    implemented some more OOP

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -33,34 +33,50 @@ class Game
   end
 
   def done?
-    @win_comb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     @done = false
 
-    if @win_comb.any? { |elem| elem.all? { |spot| @arr[spot] == @symb} }
+    if won?
       @done = true
       puts "GAME OVER! #{@symb} wins!"
-    elsif @arr.all? { |e| e != @symb }
+    elsif full?
       @done = true
       puts "It's a draw!"
     end
   end
 
+  def won?
+    @win_comb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    @win_comb.any? { |elem| elem.all? { |spot| @arr[spot] == @symb } }
+  end
+
+  def full?
+    @arr.all? { |e| e != ' ' }
+  end
+
   def choice
     puts 'Choose a spot: (1-9)'
     begin
-      spot = gets.match(/[1-9]/)[0]
+      @spot = gets.match(/[1-9]/)[0]
     rescue StandardError
-      puts 'Erroneous input! Try again...'
+      puts 'Wrong input, Try again!'
       retry
     else
-      @symb = if @symb == 'X'
-                'O'
-              else
-                'X'
-              end
-      place(spot.to_i)
-      board_update
+      update_game
     end
+  end
+
+  def change_symbol
+    @symb = if @symb == 'X'
+              'O'
+            else
+              'X'
+            end
+  end
+
+  def update_game
+    change_symbol
+    place(@spot.to_i)
+    board_update
   end
 end
 
