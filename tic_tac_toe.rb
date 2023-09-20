0c4077e4d03baf768821984ba99d0433cff37516
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-20 03:04:33 +0200

    renamed class to Game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Board
+class Game
   @@win_comb = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
   def initialize
@@ -54,6 +54,6 @@ class Board
   end
 end
  
-game = Board.new
+game = Game.new
 
 game.start_game
