728ae99bbb34fd6d336579099fe9a33969f8a853
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-20 14:04:28 +0200

    reordered board placement

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,11 +19,11 @@ class Game
   def board_update
     puts <<-HEREDOC
 
-       #{@arr[0]} | #{@arr[1]} | #{@arr[2]}
+       #{@arr[6]} | #{@arr[7]} | #{@arr[8]}
       ---+---+---
        #{@arr[3]} | #{@arr[4]} | #{@arr[5]}
       ---+---+---
-       #{@arr[6]} | #{@arr[7]} | #{@arr[8]}
+       #{@arr[0]} | #{@arr[1]} | #{@arr[2]}
 
     HEREDOC
   end
@@ -38,7 +38,7 @@ class Game
 
     if @win_comb.any? { |elem| elem.all? { |spot| @arr[spot] == @symb} }
       @done = true
-      puts "#{@symb} wins!"
+      puts "GAME OVER! #{@symb} wins!"
     elsif @arr.all? { |e| e != @symb }
       @done = true
       puts "It's a draw!"
