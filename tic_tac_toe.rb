796491a176d8a2dc20be5a3ab2e5ea5337d383f5
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-20 03:20:53 +0200

    renamed method and vars, formatted code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Game
-  @@win_comb = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
   def initialize
     @board = "  |   |   \n----------\n  |   |  \n----------\n  |   |  \n"
     puts @board
-    @arr = Array.new(9) {' '}
+    @arr = Array.new(9) { ' ' }
     @symb = 'O'
   end
 
@@ -14,46 +12,49 @@ class Game
     puts " #{@arr[0]} | #{@arr[1]} | #{@arr[2]} \n----------\n #{@arr[3]} | #{@arr[4]} | #{@arr[5]} \n----------\n #{@arr[6]} | #{@arr[7]} | #{@arr[8]} \n"
   end
 
-  def place(num)
-    @arr[num-1] = "#{@symb}"
+  def place(spot)
+    @arr[spot - 1] = @symb
   end
 
   def start_game
-    while !@done
-      get_choice
+    until @done
+      choice
       done?
     end
   end
-  
+
   def done?
-    symb_location = @arr.each_index.select{|i| @arr[i] == "#{@symb}"}
+    @win_comb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    symb_location = @arr.each_index.select { |i| @arr[i] == @symb }
     @done = false
-    if @@win_comb.each_index.any? { |index| symb_location == @@win_comb[index] }
+    if @win_comb.each_index.any? { |index| symb_location == @win_comb[index] }
       @done = true
-    elsif @arr.all? { |e| e != ' '}
+      puts "#{@symb} wins!"
+    elsif @arr.all? { |e| e != ' ' }
       @done = true
+      puts "It's a draw!"
     end
   end
 
-  def get_choice
-    puts "Choose a spot: (1-9)"
+  def choice
+    puts 'Choose a spot: (1-9)'
     begin
-      num = Kernel.gets.match(/[1-9]/)[0]
-    rescue
-      puts "Erroneous input! Try again..."
+      spot = Kernel.gets.match(/[1-9]/)[0]
+    rescue StandardError
+      puts 'Erroneous input! Try again...'
       retry
     else
-      if @symb == 'X'
-        @symb = 'O' 
-      else
-        @symb = 'X'
-      end 
-      place(num.to_i)
+      @symb = if @symb == 'X'
+                'O'
+              else
+                'X'
+              end
+      place(spot.to_i)
       board_update
-    end 
+    end
   end
 end
- 
+
 game = Game.new
 
 game.start_game
